d310590fc471547dedeb4fc82c02e0a30cdd28d4
Ronnie Liew <theronnieliew@gmail.com>
2023-04-06 21:01:24 +0800

    Update style.ts

--- src/calendar/day/basic/style.ts
+++ src/calendar/day/basic/style.ts
@@ -16,7 +16,7 @@ export default function styleConstructor(theme: Theme = {}) {
       alignItems: 'center'
     },
     text: {
-      marginTop: constants.isAndroid ? 4 : 6,
+      marginTop: constants.isAndroid ? 4 : 2,
       fontSize: appStyle.textDayFontSize,
       fontFamily: appStyle.textDayFontFamily,
       fontWeight: appStyle.textDayFontWeight,
